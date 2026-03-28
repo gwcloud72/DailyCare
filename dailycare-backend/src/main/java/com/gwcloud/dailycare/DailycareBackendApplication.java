f89df8f8aeba43eb0b2e14d10a1abad4a3f0c8a9
gwcloud72 <uu7271gw@gmail.com>
2026-03-28 22:08:23 +0900

    백앤드 저장소 구조 정리

---
+++ dailycare-backend/src/main/java/com/gwcloud/dailycare/DailycareBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gwcloud.dailycare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailycareBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailycareBackendApplication.class, args);
+	}
+
+}
---
+++ dailycare-backend/src/test/java/com/gwcloud/demo/DailycareBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gwcloud.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailycareBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
